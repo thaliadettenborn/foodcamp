7f6a334933260d5e56a6dda932ce1d8fdb272fc5
thaliard <thalia.born@gmail.com>
2020-09-03 15:34:33 -0300

    ajustes para clean code

---
+++ js/cardapio.js
@@ -0,0 +1,72 @@
+//cardápio do foodcamp
+var pratos = [
+    {id: "p1",
+    link: "img/baiao.jpg",
+    prato: "baião de dois", 
+    descricao: "feijão de corda e arroz, acompanhando de carne de sol e queijo", 
+    preço: "19,00"},
+
+    {id: "p2",
+    link: "img/moqueca.jpg",
+    prato: "moqueca de banana da terra", 
+    descricao: "tradicional moqueca baiana na versão vegana", 
+    preço: "17,00"},
+
+    {id: "p3",
+    link: "img/tainha.jpg",
+    prato: "tainha na taquara", 
+    descricao: "tainha acompanhada de pirão e farofa de castanha do Brasil", 
+    preço: "22,00"}
+];
+
+var bebidas = [
+    {id: "b1",
+    link: "img/coca.jpg",
+    prato: "coquinha gelada", 
+    descricao: "lata 350ml", 
+    preço: "5,00"},
+
+    {id: "b2",
+    link: "img/cocaZ.jpg",
+    prato: "coquinha gelada zero", 
+    descricao: "lata 350ml", 
+    preço: "5,00"},
+
+    {id: "b3",
+    link: "img/pepsi.jpg",
+    prato: "pepsi, pode ser?", 
+    descricao: "lata 350ml", 
+    preço: "6,00"},
+
+    {id: "b4",
+    link: "img/pepsiZ.jpg",
+    prato: "pepsi zero, pode ser?", 
+    descricao: "lata 350ml", 
+    preço: "6,00"},
+
+    {id: "b5",
+    link: "img/pepsiTwist.jpg",
+    prato: "pepsi twist, pode ser?", 
+    descricao: "lata 350ml", 
+    preço: "5,00"}
+];
+
+var sobremesas = [
+    {id: "s1",
+    link: "img/pudim.jpg",
+    prato: "pudim", 
+    descricao: "pudim de leite condensado", 
+    preço: "4,00"},
+
+    {id: "s2",
+    link: "img/tiramisu.jpg",
+    prato: "tiramisù", 
+    descricao: "sobremesa de creme de chocolate e café", 
+    preço: "7,00"},
+
+    {id: "s3",
+    link: "img/tortinha.jpg",
+    prato: "torta de limão", 
+    descricao: "clássica torta de limão francesa", 
+    preço: "8,00"}
+];
--- js/menu.js
+++ js/menu.js
@@ -1,72 +1,131 @@
-//cardápio do foodcamp
-var pratos = [
-    {id: "p1",
-    link: "img/baiao.jpg",
-    prato: "baião de dois", 
-    descricao: "feijão de corda e arroz, acompanhando de carne de sol e queijo", 
-    preço: "19,00"},
+function inserirPratos(){
+    var i = 0;
+    var loop = pratos.length;
+    var sectionPratos = document.querySelector('.prato ul');
 
-    {id: "p2",
-    link: "img/moqueca.jpg",
-    prato: "moqueca de banana da terra", 
-    descricao: "tradicional moqueca baiana na versão vegana", 
-    preço: "17,00"},
+    while(i < loop){
+        var pratoElement = document.createElement("li");
 
-    {id: "p3",
-    link: "img/tainha.jpg",
-    prato: "tainha na taquara", 
-    descricao: "tainha acompanhada de pirão e farofa de castanha do Brasil", 
-    preço: "22,00"}
-];
+        //id do prato
+        pratoElement.setAttribute('id', pratos[i].id);
 
-var bebidas = [
-    {id: "b1",
-    link: "img/coca.jpg",
-    prato: "coquinha gelada", 
-    descricao: "lata 350ml", 
-    preço: "5,00"},
+        //foto do prato
+        var imgPrato = document.createElement("img");
+        imgPrato.setAttribute("src", pratos[i].link);
+        pratoElement.appendChild(imgPrato);
+        //nome do prato
+        var tituloPrato = document.createElement("h3");
+        var nomePrato = document.createTextNode(pratos[i].prato);
+        tituloPrato.appendChild(nomePrato);
+        pratoElement.appendChild(tituloPrato);
+        //descrição do prato
+        var descricaoPrato = document.createElement("p");
+        var textDescricao = document.createTextNode(pratos[i].descricao);
+        descricaoPrato.appendChild(textDescricao);
+        pratoElement.appendChild(descricaoPrato);
+        //preço do prato
+        var precoPrato = document.createElement("h5");
+        var textPreco = document.createTextNode("R$ " + pratos[i].preço);
+        precoPrato.appendChild(textPreco);
+        pratoElement.appendChild(precoPrato);
+        //icone de check
+        var iconeCheck = document.createElement('ion-icon');
+        iconeCheck.setAttribute('name','checkmark-circle');
+        iconeCheck.setAttribute('class','iconeCheck');
+        pratoElement.appendChild(iconeCheck);
 
-    {id: "b2",
-    link: "img/cocaZ.jpg",
-    prato: "coquinha gelada zero", 
-    descricao: "lata 350ml", 
-    preço: "5,00"},
+        //adicionando função seleção do item li
+        pratoElement.setAttribute('onclick', 'selecao(pratos, ' + i +')');
 
-    {id: "b3",
-    link: "img/pepsi.jpg",
-    prato: "pepsi, pode ser?", 
-    descricao: "lata 350ml", 
-    preço: "6,00"},
+        sectionPratos.appendChild(pratoElement);
+        i++;
+    };
+};
 
-    {id: "b4",
-    link: "img/pepsiZ.jpg",
-    prato: "pepsi zero, pode ser?", 
-    descricao: "lata 350ml", 
-    preço: "6,00"},
+function inserirBebidas(){
+    var i = 0;
+    var loop = bebidas.length;
+    var sectionBebida = document.querySelector('.bebida ul');
 
-    {id: "b5",
-    link: "img/pepsiTwist.jpg",
-    prato: "pepsi twist, pode ser?", 
-    descricao: "lata 350ml", 
-    preço: "5,00"}
-];
+    while(i < loop){
+        var bebidaElement = document.createElement("li");
 
-var sobremesas = [
-    {id: "s1",
-    link: "img/pudim.jpg",
-    prato: "pudim", 
-    descricao: "pudim de leite condensado", 
-    preço: "4,00"},
+        //id da bebida
+        bebidaElement.setAttribute('id', bebidas[i].id);
 
-    {id: "s2",
-    link: "img/tiramisu.jpg",
-    prato: "tiramisù", 
-    descricao: "sobremesa de creme de chocolate e café", 
-    preço: "7,00"},
+        //foto da bebida
+        var imgBebida = document.createElement("img");
+        imgBebida.setAttribute("src", bebidas[i].link);
+        bebidaElement.appendChild(imgBebida);
+        //nome da bebida
+        var tituloBebida = document.createElement("h3");
+        var nomeBebida = document.createTextNode(bebidas[i].prato);
+        tituloBebida.appendChild(nomeBebida);
+        bebidaElement.appendChild(tituloBebida);
+        //descrição da bebida
+        var descricaoBebida = document.createElement("p");
+        var textBebDescricao = document.createTextNode(bebidas[i].descricao);
+        descricaoBebida.appendChild(textBebDescricao);
+        bebidaElement.appendChild(descricaoBebida);
+        //preço da bebida
+        var precoBebida = document.createElement("h5");
+        var textPrecoBebida = document.createTextNode("R$ " + bebidas[i].preço);
+        precoBebida.appendChild(textPrecoBebida);
+        bebidaElement.appendChild(precoBebida);
+        //icone de check
+        var iconeCheck = document.createElement('ion-icon');
+        iconeCheck.setAttribute('name','checkmark-circle');
+        iconeCheck.setAttribute('class','iconeCheck');
+        bebidaElement.appendChild(iconeCheck);
 
-    {id: "s3",
-    link: "img/tortinha.jpg",
-    prato: "torta de limão", 
-    descricao: "clássica torta de limão francesa", 
-    preço: "8,00"}
-];
+        //adicionando função seleção do item li
+        bebidaElement.setAttribute('onclick', 'selecao(bebidas, ' + i +')');
+
+        sectionBebida.appendChild(bebidaElement);
+        i++;
+    };
+};
+
+
+function inserirSobremesas(){
+    var i = 0;
+    var loop = sobremesas.length;
+    var sectionSobremesa = document.querySelector('.sobremesa ul');
+
+    while(i < loop){
+        var sobremesaElement = document.createElement("li");
+
+        //id da bebida
+        sobremesaElement.setAttribute('id', sobremesas[i].id);
+
+        //foto da sobremesa
+        var imgSobremesa = document.createElement("img");
+        imgSobremesa.setAttribute("src", sobremesas[i].link);
+        sobremesaElement.appendChild(imgSobremesa);
+        //nome da sobremesa
+        var tituloSobremesa = document.createElement("h3");
+        var nomeSobremesa = document.createTextNode(sobremesas[i].prato);
+        tituloSobremesa.appendChild(nomeSobremesa);
+        sobremesaElement.appendChild(tituloSobremesa);
+        //descrição da sobremesa
+        var descricaoSobremesa = document.createElement("p");
+        var textSobremesaDescricao = document.createTextNode(sobremesas[i].descricao);
+        descricaoSobremesa.appendChild(textSobremesaDescricao);
+        sobremesaElement.appendChild(descricaoSobremesa);
+        //preço da sobremesa
+        var precoSobremesa = document.createElement("h5");
+        var textPrecoSobremesa = document.createTextNode("R$ " + sobremesas[i].preço);
+        precoSobremesa.appendChild(textPrecoSobremesa);
+        sobremesaElement.appendChild(precoSobremesa);
+        //icone de check
+        var iconeCheck = document.createElement('ion-icon');
+        iconeCheck.setAttribute('name','checkmark-circle');
+        iconeCheck.setAttribute('class','iconeCheck');
+        sobremesaElement.appendChild(iconeCheck);
+
+        //adicionando função seleção do item li
+        sobremesaElement.setAttribute('onclick', 'selecao(sobremesas, ' + i +')');
+        sectionSobremesa.appendChild(sobremesaElement);
+        i++;
+    };
+};
