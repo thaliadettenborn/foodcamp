1358729ed343a99e2eae0fc2f43f9bdaa2581b12
thaliadettenborn <thalia.born@gmail.com>
2020-10-06 18:50:48 -0300

    refactor restaurant menu to object

--- src/componentes/restaurantMenu.js
+++ src/componentes/restaurantMenu.js
@@ -1,88 +1,74 @@
-//-----------------------------------------------cardápio do foodcamp-----------------------------------------------
-/*obs:
-
-1. caso seja inserido mais um item a qualquer menu é necessário fazer ajustes na função selecao() em main.js
-2. Ver tbm as funções que ativa a seleção num prato x e removem a seleção de outros em estilizar.js
-3. Não inserir valores com "," ex "15,50" somente com ".". O js não vai calcular o valor total se o nº tiver ","
-
-*/
-
-
-//---------------------------------refactor this:
-
-// var pratos = [
-//     {id: "p1",
-//     link: "img/baiao.jpg",
-//     prato: "baião de dois", 
-//     descricao: "feijão de corda e arroz, acompanhando de carne de sol e queijo", 
-//     preço: "19.50"},
-
-//     {id: "p2",
-//     link: "img/moqueca.jpg",
-//     prato: "moqueca de banana da terra", 
-//     descricao: "tradicional moqueca baiana na versão vegana", 
-//     preço: "17.90"},
-
-//     {id: "p3",
-//     link: "img/tainha.jpg",
-//     prato: "tainha na taquara", 
-//     descricao: "tainha acompanhada de pirão e farofa de castanha do Brasil", 
-//     preço: "21.90"}
-// ];
-
-
-// //---------------------------------bebidas:
-// var bebidas = [
-//     {id: "b1",
-//     link: "img/coca.jpg",
-//     prato: "coquinha gelada", 
-//     descricao: "lata 350ml", 
-//     preço: "5.50"},
-
-//     {id: "b2",
-//     link: "img/cocaZ.jpg",
-//     prato: "coquinha gelada zero", 
-//     descricao: "lata 350ml", 
-//     preço: "5.00"},
-
-//     {id: "b3",
-//     link: "img/pepsi.jpg",
-//     prato: "pepsi, pode ser?", 
-//     descricao: "lata 350ml", 
-//     preço: "6.50"},
-
-//     {id: "b4",
-//     link: "img/pepsiZ.jpg",
-//     prato: "pepsi zero, pode ser?", 
-//     descricao: "lata 350ml", 
-//     preço: "6.00"},
-
-//     {id: "b5",
-//     link: "img/pepsiTwist.jpg",
-//     prato: "pepsi twist, pode ser?", 
-//     descricao: "lata 350ml", 
-//     preço: "5.50"}
-// ];
-
-
-
-// //---------------------------------sobremesas:
-// var sobremesas = [
-//     {id: "s1",
-//     link: "img/pudim.jpg",
-//     prato: "pudim", 
-//     descricao: "pudim de leite condensado", 
-//     preço: "6.90"},
-
-//     {id: "s2",
-//     link: "img/tiramisu.jpg",
-//     prato: "tiramisù", 
-//     descricao: "sobremesa de creme de chocolate e café", 
-//     preço: "10.70"},
-
-//     {id: "s3",
-//     link: "img/tortinha.jpg",
-//     prato: "torta de limão", 
-//     descricao: "clássica torta de limão francesa", 
-//     preço: "12.50"}
-// ];
+export default var menu = {
+    plates: [
+        {
+            dish: "baião de dois",
+            src: "img/baiao.jpg",
+            description: "feijão de corda e arroz, acompanhando de carne de sol e queijo",
+            price: "19,50"
+        },
+        {
+            dish: "moqueca de banana da terra",
+            src: "img/moqueca.jpg",
+            description: "tradicional moqueca baiana na versão vegana",
+            price: "17,90"
+        },
+        {
+            dish: "tainha na taquara",
+            src: "img/tainha.jpg",
+            description: "tainha acompanhada de pirão e farofa de castanha do Brasil",
+            price: "21,90"
+        },
+    ],
+    beverages:[
+        {
+            beverage: "coquinha gelada",
+            src: "img/coca.jpg",
+            description: "lata 350ml",
+            price: "5,50"
+        },
+        {
+            beverage: "coquinha gelada zero",
+            src: "img/cocaZ.jpg"
+            description: "lata 350ml",
+            price: "5,00"
+        },
+        {
+            beverage: "pepsi, pode ser?",
+            src: "img/pepsi.jpg",
+            description: "lata 350ml",
+            price: "6,50"
+        },
+        {
+            beverage: "pepsi zero, pode ser?",
+            src: "img/pepsiZ.jpg",
+            description: "lata 350ml",
+            price: "6,00"
+        },
+        {
+            beverage: "pepsi twist, pode ser?",
+            src: "img/pepsiTwist.jpg",
+            description: "lata 350ml",
+            price: "5,50"
+        }
+    ],
+    desserts:[
+        {
+            dessert: "pudim",
+            src: "img/pudim.jpg",
+            description: "pudim de leite condensado",
+            price: "6,90"
+        },
+        {
+            dessert: "tiramisù",
+            src: "img/tiramisu.jpg",
+            description: "sobremesa de creme de chocolate e café",
+            price: "10,70"
+        },
+        {
+            dessert: "torta de limão",
+            src: "img/tortinha.jpg",
+            description: "clássica torta de limão francesa",
+            price: "12,50"
+        }
+    ]
+}
