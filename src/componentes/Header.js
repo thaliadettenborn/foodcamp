d9d4c87c8358398dacb4b1faa10a5f8d6c4487d1
thaliadettenborn <thalia.born@gmail.com>
2020-10-06 19:02:54 -0300

    adding header

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,5 +1,10 @@
-// <!----------------------------menu do topo--------------------------------->
-// <header>
-//     <h1>FoodCamp</h1>
-//     <h4>Sua comida em 6 minutos</h4>
-// </header>
+import React from 'react';
+
+export default function Header(){
+    return (
+        <header>
+            <h1>FoodCamp</h1>
+            <h4>Sua comida em 6 minutos</h4>
+        </header>
+    )
+}
