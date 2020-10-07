59bf830608187dba003783c24ff72ac58b5eb385
thaliadettenborn <thalia.born@gmail.com>
2020-10-06 22:31:11 -0300

    adding count on the plates

--- src/componentes/Dish.js
+++ src/componentes/Dish.js
@@ -1,8 +1,16 @@
 import React, {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 export default function Dish(props){
     var {dish,src,description,price} = props;
     var [selected,setSelection] = useState(false);
+    var [amount,setAmount] = useState(1);
+
+    if(amount === 0){
+        amount = 1;
+        selected = false;
+    }
 
     return (
         <li 
@@ -13,8 +21,15 @@ export default function Dish(props){
             <h3>{dish}</h3>
             <p>{description}</p>
             <h5>R$ {price}</h5>
+            <div className='count'>
+                <span className='red' onClick={() => setAmount(amount - 1)}>
+                    <FontAwesomeIcon icon={faMinus} />
+                </span>
+                <span>{amount}</span>
+                <span className='green' onClick={() => setAmount(amount + 1)}>
+                    <FontAwesomeIcon icon={faPlus} />
+                </span>
+            </div>
         </li>
     )
 }
-
-function 
